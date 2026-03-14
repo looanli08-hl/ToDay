94dd1e8f832e49fe755745d0967f496bf0362c11
Looan Li <Looanli08@gmail.com>
2026-03-14 21:56:18 +0800

    准备 GitHub Pages 站点内容

--- ios/ToDay/ToDay/App/AppConfiguration.swift
+++ ios/ToDay/ToDay/App/AppConfiguration.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum AppConfiguration {
     static let supportEmail: String? = "looanli08@gmail.com"
-    static let privacyPolicyURL: URL? = URL(string: "https://looanli08-hl.github.io/today/privacy.html")
-    static let termsOfServiceURL: URL? = URL(string: "https://looanli08-hl.github.io/today/terms.html")
-    static let websiteURL: URL? = URL(string: "https://looanli08-hl.github.io")
+    static let privacyPolicyURL: URL? = URL(string: "https://looanli08-hl.github.io/ToDay/privacy.html")
+    static let termsOfServiceURL: URL? = URL(string: "https://looanli08-hl.github.io/ToDay/terms.html")
+    static let websiteURL: URL? = URL(string: "https://looanli08-hl.github.io/ToDay/")
 }
